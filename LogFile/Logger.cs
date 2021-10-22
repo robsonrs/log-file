8a32efe93ddf95a57f29ebce236014901218474d
Robson Rodrigues da Silva <robson_rodriguessilva@outlook.com>
2021-10-21 21:44:20 -0300

    Primeiro commit

---
+++ LogFile/Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogFile
+{
+    internal class Logger
+    {
+        private static StreamWriter sw;
+        private static readonly string path = @"C:\Users\robso\Desktop\Log\Log.txt";
+        private static void CreateLogFile()
+        {
+            try
+            {
+                using (sw = File.CreateText(path))
+                {
+                    sw.WriteLine("DataHora;Mensagem;");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static void Log(string mensagem)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                    CreateLogFile();
+
+                using (sw = File.AppendText(path))
+                {
+                    sw.WriteLine($"{DateTime.Now};{mensagem};");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ LogFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LogFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            { 
+                for(int i = 0; i < 10; i++)
+                {
+                    Logger.Log($"Arquivo{i}");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
